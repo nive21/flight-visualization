e6fefee652eff67a672465baa836dc04c1cde8d2
Nivedhitha <nive.mathan21@gmail.com>
2024-11-25 02:30:38 -0500

    fix

--- api/fetchData.js
+++ api/fetchData.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export default async function handler(req, res) {
